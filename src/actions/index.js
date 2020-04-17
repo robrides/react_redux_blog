5f5d8286c65910d9ecb8feca886c81b70b2bedcb
Rob Lounsbury <rob.lounsbury@gmail.com>
2020-04-17 12:51:33 -0500

    Final commit for blog app

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,18 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+  // Same as above two lines using lodash's .chain
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+};
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
@@ -9,5 +23,13 @@ export const fetchPosts = () => async dispatch => {
 export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
-  dispatch({ type: 'FETCH_USER', payload: response.data })
+  dispatch({ type: 'FETCH_USER', payload: response.data });
 };
+
+// Cannot fetch the user again with this action creator, fetches once and only once
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
